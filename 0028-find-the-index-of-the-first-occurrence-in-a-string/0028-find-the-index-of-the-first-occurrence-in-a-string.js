5eefec64ec4115844b5036657dd99b4050f3c310
Nidhal10lab <nidhalpklucky@gmail.com>
2026-07-25 10:08:46 +0530

    Time: 0 ms (100%), Space: 53 MB (91.59%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+let a="sadbutsad"
+let b="sad"
+function strStr(haystack,needle){
+    let c=haystack.indexOf(needle)
+    return c;
+} 
+console.log(strStr(a,b))
