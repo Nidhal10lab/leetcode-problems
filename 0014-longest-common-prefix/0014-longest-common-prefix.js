83271f5e263cbfd8b681f7d6a11194f53d922ec1
Nidhal10lab <nidhalpklucky@gmail.com>
2026-08-01 14:57:01 +0530

    Time: 0 ms (100%), Space: 54.1 MB (59.72%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+// var longestCommonPrefix = function(strs) {
+    let a=["flower","flow","flight"] 
+function longestCommonPrefix(strs){
+ let str=strs[0];
+    for(let i=1;i<strs.length;i++){
+        while(!strs[i].startsWith(str)){
+            str=str.slice(0,-1)
+        }
+    }
+    return str
+}
+ console.log(longestCommonPrefix(a))
