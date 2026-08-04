57aa883d00c1cc62cf1e6047313756bf8d9eff28
Nidhal10lab <nidhalpklucky@gmail.com>
2026-08-04 09:39:38 +0530

    Time: 0 ms (100%), Space: 52.8 MB (92.45%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+let a=5
+let b=5
+function sum(num1, num2) {
+    let c=num1+num2;
+    return c
+};
+console.log(sum(a,b))
