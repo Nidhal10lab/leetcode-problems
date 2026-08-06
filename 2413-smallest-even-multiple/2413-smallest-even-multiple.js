dd79df51bcc569c538a2b4063848f5dae868094b
Nidhal10lab <nidhalpklucky@gmail.com>
2026-08-06 10:39:49 +0530

    Time: 0 ms (100%), Space: 53.8 MB (43.42%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let a=5
+function smallestEvenMultiple(n) {
+    if(n%2!=0){
+        let b=n*2;
+        return b
+    }
+    else{
+        let c=n;
+        return c
+    }
+}
+console.log(smallestEvenMultiple(a))
